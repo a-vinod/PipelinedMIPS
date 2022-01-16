a240b474d153754dd4e7f0a75e61e21128d7d1e6
a-vinod <arjunvinod@ucsb.edu>
2022-01-16 00:39:01 -0800

    memory stage: added pipeline registers

--- memory.v
+++ memory.v
@@ -1,20 +1,49 @@
 module memory(input             clk,
-                                RegWriteM, jumpM,
-              input      [1:0]  MemWriteM,
-              input      [4:0]  WriteRegM,
-              input      [31:0] ALUOutM, WriteDataM, PCPlus4M,
-              output reg        RegWriteW, jumpW,
-              output reg [1:0]  MemWriteW,
-              output reg [4:0]  WriteRegW,
-              output reg [31:0] ALUOutW, ReadDataW, PCPlus8W);
-    data_memory dm(.clk(clk), .WE(MemWriteM), .A(ALUOutM), .WD(WriteDataM), .RD(ReadDataW));
+                                jumpE, RegWriteE, MemWriteE,
+              input      [1:0]  MemtoRegE,
+              input      [4:0]  WriteRegE,
+              input      [31:0] ALUMultOutE, WriteDataE, PCPlus4E,
+              output reg        jumpM, RegWriteM,
+              output reg [1:0]  MemtoRegM,
+              output reg [4:0]  WriteRegM,
+              output reg [31:0] ReadDataM, ALUMultOutM, PCPlus8M);
+    data_memory dm(.clk(clk), .WE(MemWriteE), .A(ALUMultOutE), .WD(WriteDataE), .RD(ReadDataM));
 
-    assign ALUOutW   = ALUOutM;
-    assign WriteRegW = WriteRegM;
-    assign RegWriteW = RegWriteM;
-    assign RegWriteW = RegWriteM;
-    assign jumpW     = jumpM;
-    assign MemWriteW = MemWriteM;
+    // Pipeline registers
+    reg        jumpM_, RegWriteM_;
+    reg [1:0]  MemtoRegM_;
+    reg [4:0]  WriteRegM_;
+    reg [31:0] ALUMultOutM_, WriteDataM_, PCPlus4M_;
 
-    assign PCPlus8W  = PCPlus4M + 4;
+    assign jumpM      = jumpM_;
+    assign RegWriteM  = RegWriteM_;
+    assign MemtoRegM  = MemtoRegM_;
+    assign WriteRegM  = WriteRegM_;
+    assign WriteDataM = WriteDataM_;
+    assign PCPlus4M   = PCPlus4M_;
+
+    assign PCPlus8M = PCPlus4M_ + 4;
+    
+    always @ (posedge clk or posedge rst) begin
+        if (rst) begin
+            jumpM_     <= 0;
+            RegWriteM_ <= 0;
+            MemtoRegM_ <= 0;
+
+            ALUMultOutM_ <= 0;
+            WriteDataM_  <= 0;
+            WriteRegM_   <= 0;
+            PCPlus4M_    <= 0;
+        end else begin
+            jumpM_     <= jumpE;
+            RegWriteM_ <= RegWriteE;
+            MemtoRegM_ <= MemtoRegE;
+
+            ALUMultOutM_ <= ALUMultOutE;
+            WriteDataM_  <= WriteDataE;
+            WriteRegM_   <= WriteRegE;
+            PCPlus4M_    <= PCPlus4E;
+        end
+    end
+
 endmodule
