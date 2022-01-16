320cdc5b73ec11682faeb8097db8adc7be2ffa33
a-vinod <arjunvinod@ucsb.edu>
2022-01-16 01:25:04 -0800

    writeback stage without testing

--- writeback.v
+++ writeback.v
@@ -1,9 +1,42 @@
-module writeback(input             RegWriteW, jumpW, 
-                 input      [1:0]  MemWriteW,
-                 input      [4:0]  WriteRegW_in,
-                 input      [31:0] ReadDataW, ALUOutW, PCPlus8W,
-                 output reg [4:0]  WriteRegW,
-                 output reg [31:0] ResultW);
-    assign ResultW   = MemWriteW[1] ? (MemWriteW[0] ? ReadDataW : ALUOutW) : (PCPlus8W);
-    assign WriteRegW = jumpW        ? (5'b11111)                           : WriteRegW_in;
+module writeback(input             jumpM, RegWriteM,
+                 input      [1:0]  MemtoRegM,
+                 input      [4:0]  WriteRegM,
+                 input      [31:0] ReadDataM, ALUMultOutM, PCPlus8M,
+
+                 input             PCSrcD, jumpD,
+                 input      [27:0] jumpDstD
+                 input      [31:0] PCPlus4F, PCBranchD,
+                 
+                 output reg        RegWriteW, WriteRegW,
+                 output reg [31:0] ResultW,
+                 output reg [31:0] PC, WriteRegW);
+    // Pipeline registers
+    reg RegWriteW_, jumpW_;
+    reg [4:0]  WriteRegW_;
+    reg [31:0] ReadDataW_, ALUMultOutW_, PCPlus8W_;
+
+    assign RegWriteW = RegWriteW_;
+    assign ResultW = MemtoRegM[1] ? (MemtoRegM[0] ? (ReadDataW_) : (ALUMultOutW_)) : (PCPlus8W_);
+    assign WriteRegW = jumpW_     ? (WriteRegW_) : (5'b11111);
+    assign PC = jumpD ? ({PCPlus4F[31:28], jumpDstD}) : (PCSrcD ? (PCBranchD) : (PCPlus4F));
+
+    always @ (posedge clk or posedge rst) begin
+        if (rst) begin
+            RegWriteW_   <= 0;
+            jumpW_       <= 0;
+            WriteRegW_   <= 0;
+            ReadDataW  _ <= 0; 
+            ALUMultOutW_ <= 0;
+            PCPlus8W_    <= 0;
+        end else begin
+            RegWriteW_   <= RegWriteM;
+            jumpW_       <= jumpM;
+            WriteRegW_   <= WriteRegM;
+            ReadDataW  _ <= ReadDataM; 
+            ALUMultOutW_ <= ALUMultOutM;
+            PCPlus8W_    <= PCPlus8M;
+        end
+    end
+
+
 endmodule
---
+++ writeback_tb.v
@@ -0,0 +1,35 @@
+`timescale 1 ns/10 ps  // time-unit = 1 ns, precision = 10 ps
+
+module writeback_tb;
+    reg 		clk, rst;
+	reg         jumpM, RegWriteM;
+  	reg  [1:0]  MemtoRegM;
+  	reg  [4:0]  WriteRegM;
+  	reg  [31:0] ReadDataM, ALUMultOutM, PCPlus8M;
+    reg         PCSrcD, jumpD;
+  	reg  [27:0] jumpDstD;
+  	reg  [31:0] PCPlus4F, PCBranchD;
+	wire        RegWriteW;
+  	wire [31:0] ResultW;
+  	wire [31:0] PC, WriteRegW;
+    integer i;
+  
+  	writeback dut(clk, rst, jumpM, RegWriteM, MemtoRegM, WriteRegM, ReadDataM, ALUMultOutM, PCPlus8M, PCSrcD, jumpD, jumpDstD, PCPlus4F, PCBranchD, RegWriteW, ResultW, PC, WriteRegW);
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(1);
+      	clk = 0;
+      	rst = 0;
+        #5 clk = !clk;
+        rst = 0;
+        #5 clk = !clk;
+        rst = 1;
+        #5 clk = !clk;
+        rst = 0;
+ 
+      
+      	for (i = 0; i < 10; i = i + 1) begin
+            #5 clk = !clk;
+        end
+    end
+endmodule
