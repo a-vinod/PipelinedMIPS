53f85eaec427ffe9442d490888759c9a7d972f27
a-vinod <arjunvinod@ucsb.edu>
2022-02-03 19:24:39 -0800

    modified data memory to interface with cache

--- modules/data_memory.v
+++ modules/data_memory.v
@@ -1,12 +1,37 @@
-module data_memory(input             clk, WE,
-                   input      [31:0] A, WD,
-                   output reg [31:0] RD);
-  reg [31:0] dm[63:0];
-  // Initialize external memory to zero
-  always @ (posedge clk) begin
-    case (WE)
-      0: RD    <= dm[A];
-      1: dm[A] <= WD;
-    endcase
-  end
+module data_memory(input              clk, rst, WE,
+                   input      [31:0]  A, WD,
+                   output reg		  READY,
+                   output reg [127:0] RD);
+    reg [31:0] dm[63:0];
+    reg [5:0] delay;
+
+    // DEBUG WIRES
+    wire [31:0] dm_a;
+    assign dm_a = dm[A];
+
+    // Initialize external memory to zero
+    integer i;
+    always @ (posedge clk, posedge rst) begin
+        if (rst) begin
+          	delay = 6'b0;
+          	READY <= 0;
+            for (i = 0; i < 64; i = i + 1)
+              	dm[i] = 32'b0;
+        end else begin
+          	if (WE) begin
+              	READY <= 0;
+                dm[A] <= WD;
+            end else begin
+                // 20 cycle delay
+                if (delay < 19) begin
+                    delay <= delay + 1;
+                    READY <= 0;
+                end else begin
+                    delay <= 0;
+                    RD    <= {dm[{A[31:4], 4'b1100}], dm[{A[31:4], 4'b1000}], dm[{A[31:4], 4'b0100}], dm[{A[31:4], 4'b0000}]};
+                    READY <= 1;
+                end
+            end
+        end
+    end
 endmodule
