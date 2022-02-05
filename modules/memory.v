b93b96c2e7f6c8c02d5506b519e19d578397739a
a-vinod <arjunvinod@ucsb.edu>
2022-02-05 02:00:04 -0800

    memory stage stall implementation

--- modules/memory.v
+++ modules/memory.v
@@ -1,27 +1,44 @@
-module memory(input             clk, rst,
+module memory(input             clk, rst, stallM,
                                 jumpE, RegWriteE, MemWriteE,
               input      [3:0]  MemtoRegE,
               input      [4:0]  WriteRegE,
               input      [31:0] ALUMultOutE, WriteDataE, PCPlus4E,
-              output reg        jumpM, RegWriteM, hitM,
-              output reg [3:0]  MemtoRegM,
-              output reg [4:0]  WriteRegM,
-              output reg [31:0] ALUMultOutM,
+              output         jumpM, RegWriteM, hitM,
+              output  [3:0]  MemtoRegM,
+              output  [4:0]  WriteRegM,
+              output  [31:0] ALUMultOutM,
               output     [31:0] ReadDataM,
-              output reg [31:0] PCPlus8M);  
-
+              output  [31:0] PCPlus8M); 
 
     wire         data_memory_ready;
     wire [127:0] data_memory_RD;
-    data_memory dm (clk, rst, MemWriteE, ALUMultOutE, WriteDataE, data_memory_ready, data_memory_RD);
-    d_cache     dc (clk, rst, ALUMultOutE, data_memory_RD, data_memory_ready, hitM, ReadDataM);
-	always @ (posedge clk) begin
-		ALUMultOutM <= ALUMultOutE;
-		WriteRegM <= WriteRegE;
-		MemtoRegM <= MemtoRegE;
-		jumpM <= jumpE;
-		RegWriteM <= RegWriteE;
-		PCPlus8M <= PCPlus4E + 4;
+    
+    reg        jumpE_, RegWriteE_, MemWriteE_;
+    reg [3:0]  MemtoRegE_;
+    reg [4:0]  WriteRegE_;
+    reg [31:0] ALUMultOutE_, WriteDataE_, PCPlus4E_;
+    
+    assign jumpM = jumpE_;
+    assign RegWriteM = RegWriteE_;
+    assign MemtoRegM = MemtoRegE_;
+    assign WriteRegM = WriteRegE_;
+    assign ALUMultOutM = ALUMultOutE_;
+    assign PCPlus8M = PCPlus4E_ + 4;
+
+		d_cache     dc (clk, rst, MemWriteE_, MemtoRegE_, ALUMultOutE_, WriteDataE_, data_memory_RD, data_memory_ready, hitM, ReadDataM);
+		data_memory dm (clk, rst, MemWriteE_, MemtoRegE_, ALUMultOutE_, WriteDataE_, data_memory_ready, data_memory_RD);
+		always @ (posedge clk) begin
+				if (!stallM) begin
+        		jumpE_ <= jumpE;
+        		RegWriteE_ <= RegWriteE;
+       		  MemtoRegE_ <= MemtoRegE;
+        		WriteRegE_ <= WriteRegE;
+        		ALUMultOutE_ <= ALUMultOutE;
+        		PCPlus4E_ <= PCPlus4E;
+
+        		WriteDataE_<= WriteDataE;
+        		MemWriteE_ <= MemWriteE;
+				end
 	end
 
 endmodule
