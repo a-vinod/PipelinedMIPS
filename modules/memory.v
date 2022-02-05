2a911b52e3fcc280b150e52d6355fdb839f1e081
a-vinod <arjunvinod@ucsb.edu>
2022-02-05 15:56:23 -0800

    minor change to memory stage formatting

--- modules/memory.v
+++ modules/memory.v
@@ -1,18 +1,19 @@
-module memory(input             clk, rst, stallM,
-                                jumpE, RegWriteE, MemWriteE,
-              input      [3:0]  MemtoRegE,
-              input      [4:0]  WriteRegE,
-              input      [31:0] ALUMultOutE, WriteDataE, PCPlus4E,
-              output         jumpM, RegWriteM, hitM,
-              output  [3:0]  MemtoRegM,
-              output  [4:0]  WriteRegM,
-              output  [31:0] ALUMultOutM,
-              output     [31:0] ReadDataM,
-              output  [31:0] PCPlus8M); 
+module memory(input         clk, rst, stallM,
+                            jumpE, RegWriteE, MemWriteE,
+              input  [3:0]  MemtoRegE,
+              input  [4:0]  WriteRegE,
+              input  [31:0] ALUMultOutE, WriteDataE, PCPlus4E,
+              output        jumpM, RegWriteM, hitM,
+              output [3:0]  MemtoRegM,
+              output [4:0]  WriteRegM,
+              output [31:0] ALUMultOutM,
+              output [31:0] ReadDataM,
+              output [31:0] PCPlus8M); 
 
     wire         data_memory_ready;
     wire [127:0] data_memory_RD;
     
+		// Pipeline registers updated on the rising edge
     reg        jumpE_, RegWriteE_, MemWriteE_;
     reg [3:0]  MemtoRegE_;
     reg [4:0]  WriteRegE_;
@@ -29,16 +30,15 @@ module memory(input             clk, rst, stallM,
 		data_memory dm (clk, rst, MemWriteE_, MemtoRegE_, ALUMultOutE_, WriteDataE_, data_memory_ready, data_memory_RD);
 		always @ (posedge clk) begin
 				if (!stallM) begin
-        		jumpE_ <= jumpE;
-        		RegWriteE_ <= RegWriteE;
-       		  MemtoRegE_ <= MemtoRegE;
-        		WriteRegE_ <= WriteRegE;
+        		jumpE_       <= jumpE;
+        		RegWriteE_   <= RegWriteE;
+       		  MemtoRegE_   <= MemtoRegE;
+        		WriteRegE_   <= WriteRegE;
         		ALUMultOutE_ <= ALUMultOutE;
-        		PCPlus4E_ <= PCPlus4E;
+        		PCPlus4E_    <= PCPlus4E;
 
-        		WriteDataE_<= WriteDataE;
-        		MemWriteE_ <= MemWriteE;
+        		WriteDataE_  <= WriteDataE;
+        		MemWriteE_   <= MemWriteE;
 				end
-	end
-
+		end
 endmodule
